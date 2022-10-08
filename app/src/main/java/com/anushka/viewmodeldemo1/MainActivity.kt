9035d9c880565896f7d0982aac856f53a99351e9
seunggyu <seunggyu97@gmail.com>
2022-10-08 13:58:39 +0900

    LiveData with DataBinding

--- app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
+++ app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -14,10 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.lifecycleOwner = this
         binding.myViewModel = viewModel
-        viewModel.count.observe(this, Observer {
-          binding.countText.text = it.toString()
-        })
         
     }
 }
